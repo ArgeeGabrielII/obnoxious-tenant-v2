102ba65e5851d3c17051640bedbd0eae30282cd3
argeegabrielii <rosalitogabrielii@gmail.com>
2021-08-21 01:22:50 +0800

    Env OK

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
     production: true,
 
-    GRAPHQL_ENDPOINT: `http://35.221.225.40/v1/graphql`,
-    GRAPHQL_ENDPOINT_AUTH: `9N6#66yztPB!`,
     getLoginDetails: `https://asia-east2-crested-display-322005.cloudfunctions.net/checkLogin`,
     getUserToken: `https://asia-east2-crested-display-322005.cloudfunctions.net/generateToken`,
     crypt: `https://asia-east2-crested-display-322005.cloudfunctions.net/crypto`,
